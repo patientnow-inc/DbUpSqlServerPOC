47cbc07d9d801a4ee75438e4f2e555733551287c
Saravanakumar-pn <ssundarra@patientnow.com>
2025-05-21 18:10:01 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,39 @@ resource "aws_s3_bucket" "dbup_artifacts" {
   bucket = "dbup-artifacts-bucket"
 }
 
+resource "aws_codebuild_project" "dbup_project" {
+  name          = "dbup-project"
+  description   = "Build project for running DbUp migrations"
+
+  source {
+    type      = "S3"
+    location  = "${aws_s3_bucket.dbup_artifacts.bucket}/"
+    buildspec = "buildspec.yml"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    environment_variables = [
+      {
+        name  = "DOTNET_ROOT"
+        value = "/usr/share/dotnet"
+      }
+    ]
+  }
+
+  service_role = aws_iam_role.codebuild_service_role.arn
+}
+
 resource "aws_iam_role" "codebuild_service_role" {
   name = "codebuild-dbup-service-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
       Principal = {
         Service = "codebuild.amazonaws.com"
       }
@@ -21,31 +46,7 @@ resource "aws_iam_role" "codebuild_service_role" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild_attach_policy" {
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   role       = aws_iam_role.codebuild_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_codebuild_project" "dbup_project" {
-  name          = "dbup-project"
-  service_role  = aws_iam_role.codebuild_service_role.arn
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:7.0"
-    type                        = "LINUX_CONTAINER"
-    environment_variables = [
-      {
-        name  = "CONNECTION_STRING"
-        value = "Server=mydb;Database=test;User Id=sa;Password=pass;"
-      }
-    ]
-  }
-  source {
-    type      = "S3"
-    location  = "${aws_s3_bucket.dbup_artifacts.bucket}/publish.zip"
-    buildspec = "buildspec.yml"
-  }
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
