9f329d58bab4d18678110d339f75c09f78c7123c
Saravanakumar-pn <ssundarra@patientnow.com>
2025-05-22 11:49:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,58 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
+# --- S3 Bucket for Artifacts ---
 resource "aws_s3_bucket" "dbup_artifacts" {
   bucket = "dbup-artifacts-bucket"
+
+  versioning {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "DbUp Artifacts"
+    Environment = "Dev"
+  }
 }
 
+# --- IAM Role for CodeBuild ---
+resource "aws_iam_role" "codebuild_service_role" {
+  name = "codebuild-dbup-service-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codebuild.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# --- IAM Policy Attachment for CodeBuild Role ---
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_service_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+# --- CodeBuild Project ---
 resource "aws_codebuild_project" "dbup_project" {
-  name          = "dbup-project"
-  description   = "Build project for running DbUp migrations"
+  name        = "dbup-project"
+  description = "Build project for running DbUp migrations"
 
   source {
     type      = "S3"
-    location  = "${aws_s3_bucket.dbup_artifacts.bucket}/"
+    location  = "${aws_s3_bucket.dbup_artifacts.bucket}/dbup.zip" # <- Object key is needed
     buildspec = "buildspec.yml"
+  }
+
+  artifacts {
+    type = "NO_ARTIFACTS"
   }
 
   environment {
@@ -29,24 +68,4 @@ resource "aws_codebuild_project" "dbup_project" {
   }
 
   service_role = aws_iam_role.codebuild_service_role.arn
-}
-
-resource "aws_iam_role" "codebuild_service_role" {
-  name = "codebuild-dbup-service-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = {
-        Service = "codebuild.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "codebuild_policy" {
-  role       = aws_iam_role.codebuild_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
